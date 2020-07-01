91af54cb5fe99c75e09d1b55db1629d980ca67f3
mycodesrb <62849687+mycodesrb@users.noreply.github.com>
2020-07-02 00:29:37 +0530

    Update CNN_Implementation_Keras_CIFAR10_Dataset_Model_Building_And_Predicting.py

--- CNN_Implementation_Keras_CIFAR10_Dataset_Model_Building_And_Predicting.py
+++ CNN_Implementation_Keras_CIFAR10_Dataset_Model_Building_And_Predicting.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
-
-
 # Here I have mplemented CNN on the CIFAR-10 dataset. It is a collection of images that are commonly used to train machine
 # learning and computer vision algorithms. It is one of the most widely used datasets for machine learning research.
 # The CIFAR-10 dataset contains 60,000 32x32 color images in 10 different classes. The 10 different classes represent
 # airplanes, cars, birds, cats, deer, dogs, frogs, horses, ships, and trucks. 50,000 images for training and 10,000 for test
-
-
-# In[25]:
-
 
 # import required package
 import pandas as pd
@@ -19,35 +12,15 @@ import numpy as np
 from keras.datasets import cifar10
 import matplotlib.pyplot as plt
 
-
-# In[26]:
-
-
 # Load the dataset
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-
-# In[27]:
-
-
 print(X_train.shape, y_train.shape)
-
-
-# In[28]:
-
 
 print(X_test.shape, y_test.shape)
 
-
-# In[48]:
-
-
 # Output classes
 np.unique(y_test)
-
-
-# In[29]:
-
 
 # We will follow the general practice of reshape the inputs and hot encoding the outputs
 print("Before Reshaping: ", np.min(X_train), np.max(X_train))
@@ -55,32 +28,16 @@ X_train1 = X_train.reshape(50000, 32,32, 3).astype("float32")
 X_train1 = X_train1/255
 print("After Reshaping: ", np.min(X_train1), np.max(X_train1))
 
-
-# In[30]:
-
-
 X_test1 = X_test.reshape(10000, 32,32,3).astype('float32')
 X_test1 = X_test1/255
 print("After Reshaping: ", np.min(X_test1), np.max(X_test1))
 
-
-# In[31]:
-
-
 # Hot Encode Output
 from keras.utils import np_utils
-
-
-# In[32]:
-
 
 y_train1 = np_utils.to_categorical(y_train)
 y_test1 = np_utils.to_categorical(y_test)
 print(y_train.shape, y_test.shape)
-
-
-# In[33]:
-
 
 # Import required libs for building the CNN model
 from keras.models import Sequential
@@ -88,10 +45,6 @@ from keras.layers import Dense
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers import Flatten
-
-
-# In[34]:
-
 
 # Build the CNN model
 model =  Sequential()
@@ -110,46 +63,21 @@ model.add(Dense(10, activation='softmax')) # for 10 output classes
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-
-# In[36]:
-
-
 # Fit data
 hist = model.fit(X_train1, y_train1, validation_data=(X_test1, y_test1), epochs=2, verbose=1)
-
-
-# In[38]:
-
 
 # check accuracy and loss
 print(hist.history['accuracy'])
 print(hist.history['loss'])
 
-
-# In[49]:
-
-
 # predict
 res = np.argmax(model.predict(X_test1[17:18]))
 res
 
-
-# In[41]:
-
-
 # Check
 plt.imshow(X_test[17])
-
-
-# In[42]:
-
 
 # verify
 y_test[17]
 
-
-# In[ ]:
-
-
-
-
+Bingo..!
